d945dbcd0eb791a9faddcd0007b0f804ae2fc254
adnan <adnanfahedhr@gmail.com>
2022-08-22 18:47:18 +0300

    init redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
+import store from './state/store'
 
 function App() {
   return (
-    <>
-    </>
+    <StrictMode>
+      <Provider store={store}>
+        <></>
+      </Provider>
+    </StrictMode>
   );
 }
 
